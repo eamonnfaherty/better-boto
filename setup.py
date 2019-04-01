fdc5a63e2d015b4ff2c2cfd29818fe45702da794
Eamonn Faherty <eamonnf@amazon.co.uk>
2019-04-01 15:41:40 +0100

    v 0.1.1

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="better-boto",
-    version="0.1.0",
+    version="0.1.1",
     author="Eamonn Faherty",
     author_email="python-packages@designandsolve.co.uk",
     description="Helpers to make using boto3 more enjoyable",
