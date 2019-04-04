34a0c5d22a869f3f3fd30f95d3e69979e4beb3c8
Eamonn Faherty <eamonnf@amazon.co.uk>
2019-04-04 08:45:22 +0100

    adding to travisci

--- betterboto/organizations.py
+++ betterboto/organizations.py
@@ -27,8 +27,7 @@ def list_children_nested(self, **kwargs):
         response = self.list_children_single_page(ParentId=parent_id, ChildType='ORGANIZATIONAL_UNIT')
         my_org_children = response.get('Children')
         for my_org_child in my_org_children:
-            response = self.list_children_nested(ParentId=my_org_child.get('Id'), ChildType='ACCOUNT')
-            my_account_children += response.get('Children')
+            my_account_children += self.list_children_nested(ParentId=my_org_child.get('Id'), ChildType='ACCOUNT')
 
         return my_account_children
 
@@ -38,8 +37,7 @@ def list_children_nested(self, **kwargs):
         response = self.list_children_single_page(ParentId=parent_id, ChildType='ORGANIZATIONAL_UNIT')
         my_org_children = response.get('Children')
         for my_org_child in my_org_children:
-            response = self.list_children_nested(ParentId=my_org_child.get('Id'), ChildType='ORGANIZATIONAL_UNIT')
-            my_account_children += response.get('Children')
+            my_account_children += self.list_children_nested(ParentId=my_org_child.get('Id'), ChildType='ORGANIZATIONAL_UNIT')
 
         return my_account_children
 
@@ -47,7 +45,16 @@ def list_children_nested(self, **kwargs):
         raise Exception('Unsupported ChildType: {}'.format(child_type))
 
 
+def convert_path_to_ou(self, ou):
+    response = self.list_roots()
+    for r in response.get('Roots', []):
+        r_id = r.get('Id')
+        self.list_children(ParentId=r_id, ChildType='ORGANIZATIONAL_UNIT')
+
+
+
 def make_better(client):
+    client.convert_path_to_ou = types.MethodType(convert_path_to_ou, client)
     client.list_children_single_page = types.MethodType(list_children_single_page, client)
     client.list_children_nested = types.MethodType(list_children_nested, client)
     return client
--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="better-boto",
-    version="0.5.0",
+    version="0.5.1",
     author="Eamonn Faherty",
     author_email="python-packages@designandsolve.co.uk",
     description="Helpers to make using boto3 more enjoyable",
