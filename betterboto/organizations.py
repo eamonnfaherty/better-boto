6e35712f55186dc3c37ee73b5279940bcadb66bf
Eamonn Faherty <eamonnf@amazon.co.uk>
2019-04-05 10:02:31 +0100

    fixing path expansion

--- betterboto/organizations.py
+++ betterboto/organizations.py
@@ -1,5 +1,6 @@
 import types
 import logging
+from copy import copy
 
 from .utils import slurp
 
@@ -61,21 +62,71 @@ def list_children_nested(self, **kwargs):
         raise Exception('Unsupported ChildType: {}'.format(child_type))
 
 
-def convert_path_to_ou(self, ou):
+def build_ou_tree_branch(self, parent_id):
+    logger.info("Building ou tree for: {}".format(parent_id))
+    parent_unit = self.describe_organizational_unit(OrganizationalUnitId=parent_id)
+    parent_unit['children'] = {}
+    response = self.list_children_single_page(ParentId=parent_id, ChildType='ORGANIZATIONAL_UNIT')
+    for child in response.get('Children', []):
+        child_unit = self.build_ou_tree_branch(child.get('Id'))
+        parent_unit['children'][child_unit.get('Name')] = child_unit
+    return parent_unit
+
+
+def find_match(self, parts, parent_id):
+    part_looking_for = parts.pop()
+    logger.info('Now looking for: {} in: {}'.format(part_looking_for, parent_id))
+    response = self.list_organizational_units_for_parent(ParentId=parent_id)
+    for organizational_unit in response.get('OrganizationalUnits', []):
+        logger.info("Described: {}".format(organizational_unit))
+        if organizational_unit.get('Name') == part_looking_for:
+            logger.info('Found: {}'.format(part_looking_for))
+            if len(parts) == 0:
+                logger.info('Finished looking for a match: {}'.format(organizational_unit))
+                return organizational_unit.get('Id')
+            else:
+                return self.find_match(parts, organizational_unit.get('Id'))
+
+
+def convert_path_to_ou(self, path):
     """
-    This method accepts an ou and returns the path from the root account down to the ou
+    This method accepts a path and returns the ou.
+    This raises an exception when converting / and you have more than one root
 
     :param self: organizations client
-    :param ou: the account
-    :return: the path from the root account down to the ou
+    :param path: organizations path
+    :return: the ou of the path specified
     """
-    response = self.list_roots()
-    for r in response.get('Roots', []):
-        r_id = r.get('Id')
-        self.list_children(ParentId=r_id, ChildType='ORGANIZATIONAL_UNIT')
+    logger.info("Converting: {}".format(path))
+
+    if path == "/":
+        response = self.list_roots()
+        assert len(response.get('Roots')) == 1, "You have {} roots".format(len(response.get('Roots')))
+        root = response.get('Roots')[0]
+        return root.get('Id')
+    else:
+        parts = path.split("/")
+        parts.reverse()
+        parts.pop()
+        part_looking_for = parts.pop()
+        response = self.list_roots()
+        for root in response.get('Roots', []):
+            child_id = root.get('Id')
+            response = self.list_organizational_units_for_parent(ParentId=child_id)
+            for organizational_unit in response.get('OrganizationalUnits', []):
+                if organizational_unit.get('Name') == part_looking_for:
+                    logger.debug('Found {}: in {}'.format(part_looking_for, organizational_unit.get('Id')))
+                    if len(parts) > 0:
+                        return self.find_match(parts, organizational_unit.get('Id'))
+                    else:
+                        logger.debug('Finished looking: {}'.format(organizational_unit))
+                        return organizational_unit.get('Id')
+    raise Exception("not found")
 
 
 def make_better(client):
+    client.find_match = types.MethodType(find_match, client)
+    client.build_ou_tree_branch = types.MethodType(build_ou_tree_branch, client)
     client.convert_path_to_ou = types.MethodType(convert_path_to_ou, client)
     client.list_children_single_page = types.MethodType(list_children_single_page, client)
     client.list_children_nested = types.MethodType(list_children_nested, client)
