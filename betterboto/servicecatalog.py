32e014a51e4e91fdace68141ccfa294f9757e365
Eamonn Faherty <eamonnf@amazon.co.uk>
2019-10-29 13:40:13 +0000

    adding list_accepted_portfolio_shares_single_page

--- betterboto/servicecatalog.py
+++ betterboto/servicecatalog.py
@@ -123,6 +123,22 @@ def list_launch_paths_single_page(self, **kwargs):
         **kwargs
     )
 
+def list_accepted_portfolio_shares_single_page(self, **kwargs):
+    """
+    This will continue to call list_accepted_portfolio_shares until there are no more pages left to retrieve.  It will return
+    the aggregated response in the same structure as list_accepted_portfolio_shares does.
+
+    :param self: servicecatalog client
+    :param kwargs: these are passed onto the list_accepted_portfolio_shares method call
+    :return: servicecatalog_client.list_accepted_portfolio_shares.response
+    """
+    return slurp(
+        'list_accepted_portfolio_shares',
+        self.list_accepted_portfolio_shares,
+        'PortfolioDetails',
+        **kwargs
+    )
+
 
 def make_better(client):
     client.search_products_as_admin_single_page = types.MethodType(search_products_as_admin_single_page, client)
@@ -132,4 +148,5 @@ def make_better(client):
     client.list_provisioned_product_plans_single_page = types.MethodType(list_provisioned_product_plans_single_page, client)
     client.search_provisioned_products_single_page = types.MethodType(search_provisioned_products_single_page, client)
     client.list_launch_paths_single_page = types.MethodType(list_launch_paths_single_page, client)
+    client.list_accepted_portfolio_shares_single_page = types.MethodType(list_accepted_portfolio_shares_single_page, client)
     return client
